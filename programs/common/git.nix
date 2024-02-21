ed316135cd2e3a54fa9454cf72eabffb1be6b560
Sora Liu <soraliu.dev@gmail.com>
2024-02-21 02:23:07 +0000

    feat: support git credential in git config

--- programs/common/git.nix
+++ programs/common/git.nix
@@ -4,15 +4,21 @@
     userName = "Sora Liu";
     userEmail = "soraliu.dev@gmail.com";
 
-    extraConfig = {
-      init.defaultBranch = "main";
-      push.default = "current";
-      pull.rebase = "true";
-    };
-
     aliases = {
       lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
       rg = "reflog --color --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
     };
+
+    extraConfig = {
+      init.defaultBranch = "main";
+      push.default = "current";
+      pull.rebase = "true";
+      hub.protocol = "https";
+      credential.helper = "store";
+    };
+  };
+
+  programs.gh = {
+    gitCredentialHelper.enable = true;
   };
 }
