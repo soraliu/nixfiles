712fc6694fe58572d37ef9caaed1d203c18a2d67
Sora Liu <soraliu.dev@gmail.com>
2024-03-25 21:04:20 +0800

    feat(network): Add yacd and metacubexd to clash-meta
    - Add yacd version 0.3.8 to the clash-meta package
    - Add metacubexd version 1.136.0 to the clash-meta package
    - Update external-ui-url encryption in config.enc.yaml
    - Update lastmodified and mac fields in config.enc.yaml

--- programs/common/network/clash-meta/default.nix
+++ programs/common/network/clash-meta/default.nix
@@ -1,5 +1,37 @@
 { pkgs, unstablePkgs, lib, config, useProxy, ... }: let
   cfg = config.programs.clash;
+  yacd = pkgs.stdenv.mkDerivation (finalAttrs: {
+    pname = "yacd";
+    version = "0.3.8";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "haishanh";
+      repo = "yacd";
+      rev = "09eb9389a7109eafd35118cbf7c2ac0860190b01";
+      hash = "sha256-SaVsY2kGd+v6mmjwXAHSgRBDBYCxpWDYFysCUPDZjlE=";
+    };
+
+    installPhase = ''
+      mkdir -p $out
+      cp -r ./* $out/
+    '';
+  });
+  metacubexd = pkgs.stdenvNoCC.mkDerivation (finalAttrs: {
+    pname = "metacubexd";
+    version = "1.136.0";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "MetaCubeX";
+      repo = "metacubexd";
+      rev = "630ffcc12a71e69edf647c1439dcceaaf18d2d7b";
+      hash = "sha256-v6aWhfegllqgDRdTxtMnXPzziNu1HL3XAyZOnhrInvk=";
+    };
+
+    installPhase = ''
+      mkdir -p $out
+      cp -r ./* $out/
+    '';
+  });
 in {
   imports = [
     ../../process/pm2
@@ -19,6 +51,10 @@ in {
       packages = with unstablePkgs; [
         mihomo
       ];
+      file = {
+        ".config/clash/ui/yacd".source = yacd;
+        ".config/clash/ui/metacubexd".source = metacubexd;
+      };
     };
 
     programs = {
