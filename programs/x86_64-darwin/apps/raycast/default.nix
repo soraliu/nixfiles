4eb6111966bbbdd2bdc7c7fb01dba64bf9055c3e
Sora Liu <soraliu.dev@gmail.com>
2024-04-02 02:12:32 +0800

    feat(apps): Add Raycast app installation in sudo-install-pkgs
    - Added installation command for Raycast app in sudo-install-pkgs script.

--- programs/x86_64-darwin/apps/raycast/default.nix
+++ programs/x86_64-darwin/apps/raycast/default.nix
@@ -1,16 +1,4 @@
 { pkgs, ... }: with pkgs; let
-  app = import ../app.nix rec {
-    inherit pkgs;
-
-    pname = "raycast";
-    version = "1.70.0";
-
-    src = fetchurl {
-      name = "Raycast.dmg";
-      url = "https://raycast-releases-9b5ab5b505ea.herokuapp.com/releases/${version}/download";
-      hash = "sha256-+8P8bTD4I4Se/Ii6/oHiqzqgqkrf4QNQ9RL/l9MRuO0=";
-    };
-  };
   filter = writeText "rclone-filters.txt" ''
 # NOTICE: If you make changes to this file you MUST do a --resync run.
 - Updates/**
@@ -25,8 +13,6 @@ in  {
   ];
 
   config = {
-    home.packages = [ app ];
-
     programs = {
       sops = {
         decryptFiles = [{
