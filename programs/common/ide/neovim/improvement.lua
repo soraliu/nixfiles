9ead76172d5e8a3e7b7c97706ec75fa964e4a9a2
Sora Liu <soraliu.dev@gmail.com>
2024-04-18 03:49:52 +0400

    feat(ide/neovim): Add nvim-hlslens plugin for enhanced highlighting and lensing
    - Added nvim-hlslens plugin for improved highlighting and lensing functionality in Neovim
    - Configured key mappings for nvim-hlslens plugin to enhance navigation and visibility in the editor

--- programs/common/ide/neovim/improvement.lua
+++ programs/common/ide/neovim/improvement.lua
@@ -7,6 +7,31 @@ table.insert(plugins, {
   'mg979/vim-visual-multi', -- Multiple cursors plugin
   'wellle/targets.vim', -- Expands on the idea of simple commands like di'
   {
+    'kevinhwang91/nvim-hlslens',
+    config = function()
+      require('hlslens').setup()
+
+      local opts = { noremap = true, silent = true }
+
+      vim.api.nvim_set_keymap(
+        'n',
+        'n',
+        [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
+        opts
+      )
+      vim.api.nvim_set_keymap(
+        'n',
+        'N',
+        [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
+        opts
+      )
+      vim.api.nvim_set_keymap('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], opts)
+      vim.api.nvim_set_keymap('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], opts)
+      vim.api.nvim_set_keymap('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], opts)
+      vim.api.nvim_set_keymap('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], opts)
+    end,
+  },
+  {
     'nvim-pack/nvim-spectre',
     config = function()
       require('spectre').setup({
