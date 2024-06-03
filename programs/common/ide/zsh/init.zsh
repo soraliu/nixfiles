99a746fd708eb1216fa5fba2340bc897a5a32024
Sora Liu <soraliu.dev@gmail.com>
2024-06-03 10:50:43 +0400

    feat: Add SDKMAN setup for managing Java versions
    - Added SDKMAN setup to manage Java versions
    - Updated PATH to include SDKMAN bin directory
    - Sourced SDKMAN initialization script if it exists
    - Set JAVA_HOME to the current Java version installed by SDKMAN
    - Updated PATH to include JAVA_HOME/bin

--- programs/common/ide/zsh/init.zsh
+++ programs/common/ide/zsh/init.zsh
@@ -19,3 +19,10 @@ ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=6'
 export PATH=$HOME/.nix-profile/bin:/run/current-system/sw/bin:/nix/var/nix/profiles/default/bin:$PATH
 # use commands that installed by mint, like `xcodegen`
 [ -d $HOME/.mint/bin ] && export PATH=$HOME/.mint/bin:$PATH
+
+# install sdkman: curl -s "https://get.sdkman.io" | bash
+#   sdk install java 17.0.11-zulu
+#   sdk use java 17.0.11-zulu
+export SDKMAN_DIR="$HOME/.sdkman"
+[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
+[ -e "$SDKMAN_DIR/candidates/java/current/bin" ] && export JAVA_HOME="$SDKMAN_DIR/candidates/java/current" && export PATH=$JAVA_HOME/bin:$PATH
