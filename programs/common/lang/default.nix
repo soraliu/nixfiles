5630290f938abafa309ad8eb176384c741c962e4
Sora Liu <soraliu.dev@gmail.com>
2024-07-18 10:45:59 +0400

    feat: add npmrc config

--- programs/common/lang/default.nix
+++ programs/common/lang/default.nix
@@ -1,29 +1,40 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; builtins.filter (el: el != "") [
-    (if !pkgs.stdenv.isDarwin then
-      clang_16                                # better than gcc13, provide c++ compiler
-    else "")
-    clang-tools_16                            # better than gcc13, provide c++ tools
+  config = {
+    home.packages = with pkgs; builtins.filter (el: el != "") [
+      (if !pkgs.stdenv.isDarwin then
+        clang_16                                # better than gcc13, provide c++ compiler
+      else "")
+      clang-tools_16                            # better than gcc13, provide c++ tools
 
-    nodejs_20                                 # nodejs, npm
-    yarn-berry                                # yarn berry
+      nodejs_20                                 # nodejs, npm
+      yarn-berry                                # yarn berry
 
-    python3                                   # python3
-    python311Packages.pip                     # pip3
+      python3                                   # python3
+      python311Packages.pip                     # pip3
 
-    rustc                                     # rust
-    cargo                                     # rust package maanger
+      rustc                                     # rust
+      cargo                                     # rust package maanger
 
-    stylua                                    # lua
+      stylua                                    # lua
 
-    go                                        # go
+      go                                        # go
 
-    sqlite                                    # db
-  ];
+      sqlite                                    # db
+    ];
 
-  home.sessionVariables = {
-    GOPATH = "$HOME/go";
-    ANDROID_HOME = "$HOME/Library/Android/sdk";
-    PATH = "${pkgs.nodejs_20}/bin:$GOPATH/bin:$ANDROID_HOME/emulator:$ANDROID_HOME/platform-tools:$PATH";
+    home.sessionVariables = {
+      GOPATH = "$HOME/go";
+      ANDROID_HOME = "$HOME/Library/Android/sdk";
+      PATH = "${pkgs.nodejs_20}/bin:$GOPATH/bin:$ANDROID_HOME/emulator:$ANDROID_HOME/platform-tools:$PATH";
+    };
+
+    programs = {
+      sops = {
+        decryptFiles = [{
+          from = "secrets/.npmrc.enc";
+          to = ".npmrc";
+        }];
+      };
+    };
   };
 }
