2126436fe2ecd5a0c12d7fb5aaaef4137bab39e3
Sora Liu <soraliu.dev@gmail.com>
2024-07-29 19:22:05 +0400

    feat(network): add wrk API pressure test tool

--- programs/common/network/default.nix
+++ programs/common/network/default.nix
@@ -8,6 +8,7 @@
     inetutils                                 # provide: `telnet`, `ftp`, `hostname`, `ifconfig`, `traceroute`, `ping`, etc
     wget                                      # wget
     curl                                      # curl
+    wrk                                       # API pressure test tool
     dogdns                                    # dig alternative
     bandwhich                                 # show network usage by process, need be executed by `sudo bandwhich`
                                                 # Github: https://github.com/imsnif/bandwhich
