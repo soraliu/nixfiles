ad05e070e68e164799704083e4095164ec0f756c
Sora Liu <soraliu.dev@gmail.com>
2024-08-20 00:11:28 +0400

    feat: Support install zip & gz

--- programs/common-darwin/apps/iterm2/default.nix
+++ programs/common-darwin/apps/iterm2/default.nix
@@ -1,9 +1,6 @@
 { unstablePkgs, ... }: with unstablePkgs; let
   pathToConfigLink = (builtins.getEnv "HOME") + "/.config/iterm2/com.googlecode.iterm2";
 in  {
-  config.home.packages = [
-    iterm2
-  ];
   config.programs.linker.links = [{
     source = "gdrive:Sync/Config/Darwin/com.googlecode.iterm2";
     link = pathToConfigLink;
