314c5b31bf57ddf0f91c639373a74760fddfda9b
Sora Liu <soraliu.dev@gmail.com>
2024-10-10 12:09:13 +0800

    feat: Add lazygit.nvim

--- programs/common/ide/neovim/git.lua
+++ programs/common/ide/neovim/git.lua
@@ -52,4 +52,24 @@ table.insert(plugins, {
       })
     end,
   },
+  {
+    'kdheepak/lazygit.nvim',
+    lazy = true,
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<space>vl', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
 })
