7b9cc2250d1f43f5970a86cae7c316019e89ff08
Sora Liu <soraliu.dev@gmail.com>
2024-10-31 15:58:02 +0800

    feat: Add vpn install script on ubuntu

---
+++ secrets/bin/common-linux/vpn/install-ss-on-ubuntu.sh
@@ -0,0 +1,24 @@
+{
+	"data": "ENC[AES256_GCM,data:2ilPyx1cxCHRepLs3nZdBLuT6p07Oyldcxe8jO/jfkg1P4Eqb+5SOce2QK0F7zdyVJ3GfKgJVToaKe4GefPm6QyeBPc0qNFhJjAil7LctK/90lWY+Eo1G65sKA+nlK8m9qdZFLh/6Y+izjzWMcEy74iBXkDxO3GdKNOrT22tQYcyPaIq6k1+r1Z2vOB0nLfHideXStjE2LMz8ywC2EjRu6V4oDEj1i8JhFsV510wHgkYQyeaVeh1OIU5YhVxFJLtOlts31PmkKrCKT2DVfWpnBnbLxBsiX1OYQyWxxC3GS99ZjuYeigoJSYY97mvRfA9GeONv0hyng1FggCXpVXEaaYNslL3x74WGRBHrOESmLixjZutfEGl40P2FCsfV4Ezz4ky9JXy4oXetwsDG4kqCYTu+Q7Aj/WgCJQhLOpe5NimCyEGPNVhhpaFpNWI4XjX7LpYqS6HLTXPd9LlKeaKAIVt6XVQfW33MDGc6bDqeRDAYtqWb3dOf/ki7ROMrhgS1o6VvGPdL2dpMb9D4jJIpvDlMp09dbrnUQuoZqKsGGDuFGs56MRpsbv7VXzhffIGjHMBoji26RnzjBe5zZ0SuKFWbD950qFqJG5E3oUxwT/Z0Komp0L3QziCWseDg6MuAUbHo2I8sQPypD5uiQaTueLwq/aKHML9b9y1c5sV284aokY/LcNLKvsJUMqzxNiEhZHMXSyGlsyK+/rR8DqlfrKLuG2Lrxy9BFhKK9aEI4d8RRqxWhPh+uG37tvUV0qrSA==,iv:EcBedUomPjM5pfLKdbNBb2L5SEXVbsUdphnP8xuabeg=,tag:khCiK+lrZ4B+t4fdp6Hkrg==,type:str]",
+	"sops": {
+		"kms": null,
+		"gcp_kms": null,
+		"azure_kv": null,
+		"hc_vault": null,
+		"age": [
+			{
+				"recipient": "age1gnuydcffmwalyeqr7pmgp26nnwg4smwum05atpunh3xnfz0a5cnsc6erzf",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBraGNvckZoRlBuT2JLV1JX\ndEJBQm50WlkyNDZxOHhhNFhpNnVIN3hMOHpJCkp3eVlUMnBuWG43NUxQQ01pcVVB\ndE5vN1FmZnNDZ1ZSazV6eXV1am5nVVEKLS0tIDF1cmEraURBS1lRb0Y1ejBvbWht\ncUFaS29Tck56ZVFOZlpMZ1R2d2NFakEK56+BkukEmUSaGixdh+lJvu0vyDJR7wlV\nTGJLSbumaZJ1uC1Ge+of5NKQoQevxw7X5VfrQPH7jK9Jz3HUY+bsIg==\n-----END AGE ENCRYPTED FILE-----\n"
+			},
+			{
+				"recipient": "age1efdgqxst6zk3vysvnmrexuh9csqwz9nwhtr9uyp77xn8epddvcuskq8y9v",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBoVS9jSGk1alVZWkVmU0FH\nV1lqR1BBbmwzbzMwQVZQREZUZ09DbkRlblZFCkRUNXErUmpoNmxCeDZSaHI4ZHVm\ncGQzQ2F1YmZPQWRiaVVCRkdZa3crVVUKLS0tIHJrbXBFY1NSbHF4RnMxRTBLMFdw\nOCs3SFcwYXNNb0RPWHZjc3hzcjErQUkKnrl55UN6Wv8MQnsb5/Yf77v5is0jvkJW\n99vwmTNylrHDoL6xAlUIKbRLjGLnxB8vwPNYSTIGQRRzpppjDY4Acw==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2024-10-31T07:57:16Z",
+		"mac": "ENC[AES256_GCM,data:RK6tgOcy9sEIaeJoveRBbVEtbDZ0kzNgEOC/iJ9HKrLq6UEhYYvtgHQgBm+PLXA2KFilwUfHWTR7P2606bYNhA9uQdsf7bbE7jKHFWL7nU03xJ/PfhkPO1KW9JiezHdQC8KZQE/CQyJMaPrRD17Ad3ux8fbfYYBmwjNfCXPLv8o=,iv:+Nvxybe5z0H1LrfDl5pmJzEYdiSIEmHP/4qgQAkAX3M=,tag:71Fr/RnufQtl7nWkdUoncw==,type:str]",
+		"pgp": null,
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.8.1"
+	}
+}
