f14322ad6cbc23ed48338e42aded30e85a9ca3c5
Sora Liu <soraliu.dev@gmail.com>
2024-11-23 21:13:40 +0800

    feat: Add just init shell

---
+++ bin/common/pre-init-just.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+root_dir=$(git rev-parse --show-toplevel)
+path_to_just_bin=$root_dir/.local/bin/just
+
+if [ -f "$path_to_just_bin" ]; then
+  echo "Info: Just has been installed locally"
+else
+  echo "Info: Installing just"
+  curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to "$(dirname $path_to_just_bin)"
+fi
+
+path_to_just_link=/usr/local/bin/just
+if [ -f ${path_to_just_link} ]; then
+  echo "Info: just has already linked! Skip."
+else
+  # Link just to /usr/local/bin
+  echo "Info: Link ${path_to_just_bin} -> ${path_to_just_link}"
+  sudo ln -sf ${path_to_just_bin} ${path_to_just_link}
+fi
