37f4dcbc3f7b753fe2fce8ef5539dcf6d31e7ee8
Sora Liu <soraliu.dev@gmail.com>
2025-04-28 12:13:23 +0000

    feat: Create pet config file when initializing os

--- home/modules/ide/search/pet/default.nix
+++ home/modules/ide/search/pet/default.nix
@@ -6,6 +6,9 @@
       ];
 
       activation.initPet = lib.hm.dag.entryAfter ["linkGeneration"] ''
+        path_to_config="${builtins.getEnv "HOME"}/.config/pet"
+        mkdir -p "$path_to_config"
+        touch "$path_to_config/snippet.toml"
         ${pkgs.pet}/bin/pet sync
       '';
     };
