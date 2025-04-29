ebeef24b787b8dcfdbd931a35ca45cbd0105c089
Sora Liu <soraliu.dev@gmail.com>
2025-04-29 21:38:59 +0800

    feat: Add docker support

--- systems/nixos-wsl.nix
+++ systems/nixos-wsl.nix
@@ -14,4 +14,24 @@
 
   wsl.enable = true;
   # wsl.defaultUser = "root";
+
+  # docker support
+  # TL;DR: https://github.com/nix-community/NixOS-WSL/issues/235
+  wsl.extraBin = with pkgs; [
+    # Binaries for Docker Desktop wsl-distro-proxy
+    { src = "${coreutils}/bin/mkdir"; }
+    { src = "${coreutils}/bin/cat"; }
+    { src = "${coreutils}/bin/whoami"; }
+    { src = "${coreutils}/bin/ls"; }
+    { src = "${busybox}/bin/addgroup"; }
+    { src = "${su}/bin/groupadd"; }
+    { src = "${su}/bin/usermod"; }
+  ];
+  virtualisation.docker = {
+    enable = true;
+    enableOnBoot = true;
+    autoPrune.enable = true;
+  };
+  ## patch the docker script
+  systemd.services.docker-desktop-proxy.script = lib.mkForce ''${config.wsl.wslConf.automount.root}/wsl/docker-desktop/docker-desktop-user-distro proxy --docker-desktop-root ${config.wsl.wslConf.automount.root}/wsl/docker-desktop "C:\Program Files\Docker\Docker\resources"'';
 }
