b4311973e782232423928a585ef0ee1a596221c6
Sora Liu <soraliu.dev@gmail.com>
2026-03-02 21:09:58 +0800

    feat: update config

--- bin/darwin/post-install-pkgs-work.sh
+++ bin/darwin/post-install-pkgs-work.sh
@@ -22,7 +22,7 @@ install_dmg -n 'Vysor.app' -u 'https://nuts.vysor.io/download/osx' -t 'dmg' &
 install_dmg -n 'iTerm.app' -u 'https://iterm2.com/downloads/stable/iTerm2-3_5_4.zip' &
 install_dmg -n 'CompressX.app' -u 'https://drive.home.soraliu.dev/dav/software/darwin/CompressX/1.14/CompressX-1.14.dmg' &
 install_dmg -n 'Pearcleaner.app' -u 'https://github.com/alienator88/Pearcleaner/releases/download/4.4.3/Pearcleaner.dmg' &
-install_dmg -n 'CleanShot X.app' -u 'https://drive.home.soraliu.dev/dav/software/darwin/CleanShotX/4.7.6/CleanShotX.dmg' &
+install_dmg -n 'CleanShot X.app' -u 'https://drive.home.soraliu.dev/dav/software/darwin/CleanShotX/4.8.7/CleanShotX.dmg' &
 install -n 'Obsidian.app' -u 'https://github.com/obsidianmd/obsidian-releases/releases/download/v1.10.6/Obsidian-1.10.6.dmg' -t 'dmg' &
 install -n 'ClashX.Meta.app' -u 'https://github.com/MetaCubeX/ClashX.Meta/releases/download/v1.4.29/ClashX.Meta.zip' -t 'zip' &
 install -n '/Library/Input Methods/Squirrel.app' -u 'https://github.com/rime/squirrel/releases/download/1.0.3/Squirrel-1.0.3.pkg' &
--- home/ide.nix
+++ home/ide.nix
@@ -42,6 +42,7 @@ in
     # network
     dog # modern DNS client, replaces dogdns, Github: https://github.com/ogham/dog
     bandwhich # show network usage by process, need be executed by `sudo bandwhich`, Github: https://github.com/imsnif/bandwhich
+    htop-vim # better top, Github: https://github.com/KoffeinFlummi/htop-vim
 
     # doc
     tldr # community-maintained help pages, Github: https://github.com/tldr-pages/tldr
