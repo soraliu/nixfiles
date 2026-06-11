00fb7f4fb3529c874072eb13f36b2810dbf8d814
Sora Liu <soraliu.dev@gmail.com>
2026-06-11 21:53:43 +0800

    fix: run xray pm2 service without interpreter

--- home/modules/network/xray/default.nix
+++ home/modules/network/xray/default.nix
@@ -19,6 +19,7 @@
           name = "xray";
           script = "${unstablePkgs.xray}/bin/xray";
           args = "run -config ${config.home.homeDirectory}/.config/xray/config.json";
+          interpreter = "none";
           exp_backoff_restart_delay = 100;
           max_restarts = 3;
         }];
